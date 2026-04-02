b54d22d4563cc987ca5d48c1ef6c4cacbcb66870
Unasemeje ø dia <diasamratbarusz@gmail.com>
2026-04-02 19:07:48 +0300

    Implement login function with API call

---
+++ script.js
@@ -0,0 +1,23 @@
+const API_URL = "https://unasemeje.onrender.com";
+
+function login() {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  fetch(`${API_URL}/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ email, password })
+  })
+  .then(res => res.json())
+  .then(data => {
+    if (data.token) {
+      localStorage.setItem("token", data.token);
+      alert("Login successful");
+    } else {
+      alert("Login failed");
+    }
+  });
+}
