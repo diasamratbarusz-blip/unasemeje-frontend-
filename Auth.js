18b2f57421c2b2aa2adf35c9669978f375fcb503
Unasemeje ø dia <diasamratbarusz@gmail.com>
2026-04-06 20:24:24 +0300

    Create Auth.js

---
+++ Auth.js
@@ -0,0 +1,33 @@
+import { supabase } from "./supabase.js";
+
+window.loginUser = async function () {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  const { error } = await supabase.auth.signInWithPassword({
+    email,
+    password
+  });
+
+  if (error) {
+    alert(error.message);
+  } else {
+    window.location.href = "dashboard.html";
+  }
+};
+
+window.signupUser = async function () {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  const { error } = await supabase.auth.signUp({
+    email,
+    password
+  });
+
+  if (error) {
+    alert(error.message);
+  } else {
+    alert("Signup successful. Check your email.");
+  }
+};
